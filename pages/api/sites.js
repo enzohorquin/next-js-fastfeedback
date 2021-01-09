9674f5b9a988b6d7d03a6027b5bc7fa81d0d8551
Enzo Horquin <ehorquin@alumnos.exa.unicen.edu.ar>
2021-01-09 15:00:33 +0100

    Update API to show errors in production environment

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -2,12 +2,20 @@
 import db from '@/lib/firebase-admin';
 
 export default async (_, res) => {
-  const snapshot = await db.collection('sites').get();
-  const sites = [];
 
-  snapshot.forEach((doc) => {
-    sites.push({ id: doc.id, ...doc.data() });
-  });
+  try {
+    const snapshot = await db.collection('sites').get();
+    const sites = [];
 
-  res.status(200).json({ sites });
+    snapshot.forEach((doc) => {
+      sites.push({ id: doc.id, ...doc.data() });
+    });
+
+    res.status(200).json({ sites });
+  }
+  catch (error) {
+    console.log(error);
+    res.status(401).json({ sites: [] });
+  }
+
 };
